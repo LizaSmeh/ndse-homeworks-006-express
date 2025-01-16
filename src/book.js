5f1242002d631c257aed139fe6bd5c7f38c519ac
Elizaveta Smekhova <smekhovaelizaveta@gmail.com>
2025-01-16 15:48:31 +0300

    added middleware

--- src/book.js
+++ src/book.js
@@ -1,14 +1,15 @@
 const { v4: uuid } = require('uuid');
 
 class Book {
-    constructor( id =uuid(), title = "", description = "", authors = "", favorite = "", fileCover = "", fileName = "") {
+    constructor( id =uuid(), title = "", description = "", authors = "", favorite = false, fileCover = "", fileName = "", fileBook = "") {
         this.id = id,
         this.title = title,
         this.description = description,
         this.authors = authors,
         this.favorite = favorite,
         this.fileCover =fileCover,
-        this.fileName = fileName
+        this.fileName = fileName,
+        this.fileBook = fileBook
 
     }
 }
